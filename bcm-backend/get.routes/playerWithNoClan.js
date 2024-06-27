813353c5240547faf0e9cac34661cfd0700edfe4
oXoGus <mdintrat0205@gmail.com>
2024-06-27 17:29:46 +0200

    setup backend

--- bcm-backend/get.routes/playerWithNoClan.js
+++ bcm-backend/get.routes/playerWithNoClan.js
@@ -5,12 +5,11 @@ const mysql = require("mysql2")
 const router = express.Router() // on cherche l'url sur laquelle on est pour traitée les data
 
 
-dotenv.config()
 
 router.get("/", (req, res) => {
 
 
-    res.json({message : "page d'acceuil de l'api de BetterCLanMate"})
+    res.json({message : "page d'acceuil de l'api de pppp"})
 
 })
 
