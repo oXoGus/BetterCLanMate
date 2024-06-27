153579d17a8f9d52bda0723837462c84da807fb5
oXoGus <mdintrat0205@gmail.com>
2024-06-27 14:19:17 +0200

    setup backend

---
+++ bcm-app/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+ 
+export function middleware(request) {
+  let url = request.nextUrl.clone()
+  const hostname = url.pathname.startsWith("/ingest/static/") 
+  const requestHeaders = new Headers(request.headers)
+  
+  requestHeaders.set('host', hostname)
+
+  url.protocol = 'https'
+  url.hostname = hostname
+  url.port = 443
+  url.pathname = url.pathname.replace(/^\/ingest/, '');
+
+  return NextResponse.rewrite(url, {
+    headers: requestHeaders,
+  })
+}
+ 
+export const config = {
+  matcher: '/ingest/:path*',
+}
