a20744295926446c9b7af2216b8a2488e5639462
oXoGus <mdintrat0205@gmail.com>
2024-07-05 21:16:28 +0200

    update script

--- bcm-app/src/app/layout.jsx
+++ bcm-app/src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["french"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BetterClanMate",
