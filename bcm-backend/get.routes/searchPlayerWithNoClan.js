e02075f5e1783588bdeaf17b29b60e2857440961
oXoGus <mdintrat0205@gmail.com>
2024-07-09 21:05:40 +0200

    test api

--- bcm-backend/get.routes/searchPlayerWithNoClan.js
+++ bcm-backend/get.routes/searchPlayerWithNoClan.js
@@ -19,6 +19,7 @@ router.get("/:param", (req, res) => {
     axios.get(`https://api.clashofclans.com/v1/clans/%23${clanTag}`, {headers : apiHeader})
     .then((response) => {
         clanData = response.data
+        clanData = JSON.stringify(clanData)
         console.log(clanData);
         clanData = JSON.parse(clanData)
         console.log(clanData);
